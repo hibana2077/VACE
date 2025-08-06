05f53f8d0f4ee7e396a31b030960ac66f81d05e2
hibana2077 <hibana2077@gmail.com>
2025-08-06 14:04:22 +0800

    fix: Remove batch size and epochs parameters from cotton_tiny_vit training command

--- scripts/train_a100.sh
+++ scripts/train_a100.sh
@@ -18,4 +18,4 @@ cd ..
 # Run training
 # python train.py --config configs/cotton_convnext_tiny.yaml >> out_train_a100.txt
 # python train.py --config configs/cotton_r50.yaml >> out_train_a100.txt
-python train.py --config configs/cotton_tiny_vit.yaml --batch-size 64 --epochs 10 >> out_train_a100.txt
+python train.py --config configs/cotton_tiny_vit.yaml >> out_train_a100.txt
